7c11d1501a3b44c0a91f4039c126b6a4a165ec77
qMSUZ <m.sawerwain@issi.uz.zgora.pl>
2023-03-17 00:42:01 +0100

    vqe type 5 and swap-test templates

---
+++ vqe-for_credit_risk-type5.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#/***************************************************************************
+# *   Copyright (C) 2022 -- 2023 by Marek Sawerwain                         *
+# *                                  <M.Sawerwain@gmail.com>                *
+# *                                  <M.Sawerwain@issi.uz.zgora.pl>         *
+# *                                                                         *
+# *                              by Joanna Wiśniewska                       *
+# *                                  <Joanna.Wisniewska@wat.edu.pl>         *
+# *                                                                         *
+# *                                                                         *
+# *   Part of the VQEClassification:                                        *
+# *         https://github.com/qMSUZ/VQEClassification                      *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU General Public License as published by  *
+# *   the Free Software Foundation; either version 3 of the License, or     *
+# *   (at your option) any later version.                                   *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU General Public License for more details.                          *
+# *                                                                         *
+# *   You should have received a copy of the GNU General Public License     *
+# *   along with this program; if not, write to the                         *
+# *   Free Software Foundation, Inc.,                                       *
+# *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+# ***************************************************************************/
+
+import numpy as np
+import entdetector as ed
+
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
+from qiskit import Aer
+from qiskit.algorithms.optimizers import SPSA
+
+import pandas as pd
+from sympy import sqrt, I
+
+import matplotlib.pyplot as plt
+
+from sklearn import decomposition
+from sklearn.cluster import KMeans
+
+import time
+
