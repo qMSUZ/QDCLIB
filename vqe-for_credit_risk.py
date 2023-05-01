d2e4f4b21b6b23aeedd3c14c5dc31eb3aac220de
qMSUZ <m.sawerwain@issi.uz.zgora.pl>
2023-05-01 22:22:54 +0200

    a new read data functions

--- vqe-for_credit_risk.py
+++ vqe-for_credit_risk.py
@@ -46,6 +46,109 @@ from sklearn.cluster import KMeans
 
 import time
 
+
+def convert_data_to_vector_states(inputDF, cols=0):
+    """
+        Create quantum states - input data from Pandas Data Frame
+
+        Parameters
+        ----------
+        inputDF : pandas.DataFrame
+            File of input data.
+        cols : interger
+            If we would like to fetch fewer columns than the file contains,
+            this number should be assigned to the variable (if cols==0,
+            then all columns will be used).
+
+        Returns
+        -------
+        Qtab : numpy array
+            Numpy array of noemalized quantum states.
+
+        Examples
+        --------
+        From file 'name.xlsx', seven columns were fetched to produce 3-qubit states.
+        >>> df = pd.read_excel(r'name.xlsx')
+        >>> print(convert_data_to_vector_states(df,7))
+        [0.21382269 0.23794088 0.         0.08154363 0.54498154 0.54498154
+         0.54498154 0.        ]...
+    """
+    a,b=inputDF.shape
+    if cols==0:
+        print("The number of variables is:", b)
+    elif b>cols and cols>0:
+        b=cols
+        print("The number of variables is:", b)
+    else:
+        raise ValueError("The number of variables is incorrect!")
+        return None
+    #classical normalization
+    Ktab=pd.DataFrame(inputDF).to_numpy()
+    #intervals
+    maxs=np.amax(Ktab, axis=0)
+    mins=np.amin(Ktab, axis=0)
+    intervals=maxs-mins
+    KNtab=np.ndarray(shape=(a,b))
+    for j in range(b):
+        for i in range(a):
+            KNtab[i,j]=(Ktab[i,j]-mins[j])/intervals[j]
+    #quantum normalization
+    if math.ceil(math.log2(b)) != math.floor(math.log2(b)):
+        c=math.ceil(math.log2(b))
+        c=2**c
+        Qtab=np.zeros(shape=(a,c))
+    else:
+        Qtab=np.ndarray(shape=(a,b))
+    for i in range(a):
+        sum_all=0
+        for j in range(b):
+            sum_all+=KNtab[i,j]
+        for j in range(b):
+            Qtab[i,j]=sqrt(KNtab[i,j]/sum_all)
+    return Qtab
+
+
+def convert_data_to_vector_state(dataTuple):
+    """
+        Create a quantum state - parameters as input data
+
+        Parameters
+        ----------
+        dataTuple : tuple of integers or real numbers 
+            Input data as tuple of integers or real numbers.
+
+        Returns
+        -------
+        Qvec : numpy vector
+            Numpy vector containing a normalized quantum state.
+
+        Examples
+        --------
+        Create a 2-qubit state.
+        >>> print(convert_data_to_vector_state((5,4,3,1)))
+        [0.62017367 0.5547002  0.48038446 0.2773501 ]
+        Create a 3-qubit state.
+        >>> print(convert_data_to_vector_state((5,4,3,1,7)))
+        [0.5        0.4472136  0.38729833 0.2236068  0.59160798 0.
+         0.         0.        ]
+    """
+    a=len(dataTuple)
+    #quantum normalization
+    if math.ceil(math.log2(a)) != math.floor(math.log2(a)):
+        b=math.ceil(math.log2(a))
+        b=2**b
+        Qvec=np.zeros(shape=(b))
+    else:
+        Qvec=np.ndarray(shape=(a))
+    for i in range(a):
+        Qvec[i]=dataTuple[i]
+    sum_all=0
+    for i in range(a):
+        sum_all+=Qvec[i]
+    for i in range(a):
+        Qvec[i]=sqrt(Qvec[i]/sum_all)
+    return Qvec
+
 def read_data():
     df = pd.read_excel (r'data/train-data.xlsx')
     
