614c2c702b8b799d85892fedaeb190ebf0cc541b
qMSUZ <m.sawerwain@issi.uz.zgora.pl>
2023-06-10 00:49:17 +0200

    initial file for quantum distance classifier

---
+++ qdclib.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#/***************************************************************************
+# *   Copyright (C) 2022 -- 2023 by Marek Sawerwain                         *
+# *                                  <M.Sawerwain@gmail.com>                *
+# *                                  <M.Sawerwain@issi.uz.zgora.pl>         *
+# *                                                                         *
+# *                              by Joanna Wiśniewska                       *
+# *                                  <Joanna.Wisniewska@wat.edu.pl>         *
+# *                                                                         *
+# *                                                                         *
+# *   Part of the VQEClassification:                                        *
+# *         https://github.com/qMSUZ/QDCLIB                      *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU General Public License as published by  *
+# *   the Free Software Foundation; either version 3 of the License, or     *
+# *   (at your option) any later version.                                   *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU General Public License for more details.                          *
+# *                                                                         *
+# *   You should have received a copy of the GNU General Public License     *
+# *   along with this program; if not, write to the                         *
+# *   Free Software Foundation, Inc.,                                       *
+# *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+# ***************************************************************************/
+
+import numpy as np
+import pandas as pd
+import math as math
+import sympy as sympy
+
+def convert_data_to_vector_states(inputDF, cols=0):
+    """
+        Create quantum states - input data from Pandas Data Frame
+
+        Parameters
+        ----------
+        inputDF : pandas.DataFrame
+            File of input data.
+        cols : interger
+            If we would like to fetch fewer columns than the file contains,
+            this number should be assigned to the variable (if cols==0,
+            then all columns will be used).
+
+        Returns
+        -------
+        Qtab : numpy array
+            Numpy array of noemalized quantum states.
+
+        Examples
+        --------
+        From file 'name.xlsx', seven columns were fetched to produce 3-qubit states.
+        >>> df = pd.read_excel(r'name.xlsx')
+        >>> print(convert_data_to_vector_states(df,7))
+        [0.21382269 0.23794088 0.         0.08154363 0.54498154 0.54498154
+         0.54498154 0.        ]...
+    """
+    a,b=inputDF.shape
+    if cols==0:
+        print("The number of variables is:", b)
+    elif b>cols and cols>0:
+        b=cols
+        print("The number of variables is:", b)
+    else:
+        raise ValueError("The number of variables is incorrect!")
+        return None
+    # Classical normalization
+    Ktab=pd.DataFrame(inputDF).to_numpy()
+    
+    # Intervals
+    maxs=np.amax(Ktab, axis=0)
+    mins=np.amin(Ktab, axis=0)
+    intervals=maxs-mins
+    KNtab=np.ndarray(shape=(a,b))
+    for j in range(b):
+        for i in range(a):
+            KNtab[i,j]=(Ktab[i,j]-mins[j])/intervals[j]
+    #quantum normalization
+    if math.ceil(math.log2(b)) != math.floor(math.log2(b)):
+        c=math.ceil(math.log2(b))
+        c=2**c
+        Qtab=np.zeros(shape=(a,c))
+    else:
+        Qtab=np.ndarray(shape=(a,b))
+    for i in range(a):
+        sum_all=0
+        for j in range(b):
+            sum_all+=KNtab[i,j]
+        for j in range(b):
+            Qtab[i,j]=sympy.sqrt(KNtab[i,j]/sum_all)
+    return Qtab
+
+
+def convert_data_to_vector_state(dataTuple):
+    """
+        Create a quantum state - parameters as input data
+
+        Parameters
+        ----------
+        dataTuple : tuple of integers or real numbers 
+            Input data as tuple of integers or real numbers.
+
+        Returns
+        -------
+        Qvec : numpy vector
+            Numpy vector containing a normalized quantum state.
+
+        Examples
+        --------
+        Create a 2-qubit state.
+        >>> print(convert_data_to_vector_state((5,4,3,1)))
+        [0.62017367 0.5547002  0.48038446 0.2773501 ]
+        Create a 3-qubit state.
+        >>> print(convert_data_to_vector_state((5,4,3,1,7)))
+        [0.5        0.4472136  0.38729833 0.2236068  0.59160798 0.
+         0.         0.        ]
+    """
+    a=len(dataTuple)
+    #quantum normalization
+    if math.ceil(math.log2(a)) != math.floor(math.log2(a)):
+        b=math.ceil(math.log2(a))
+        b=2**b
+        Qvec=np.zeros(shape=(b))
+    else:
+        Qvec=np.ndarray(shape=(a))
+    for i in range(a):
+        Qvec[i]=dataTuple[i]
+    sum_all=0
+    for i in range(a):
+        sum_all+=Qvec[i]
+    for i in range(a):
+        Qvec[i]=sympy.sqrt(Qvec[i]/sum_all)
+    return Qvec
+
+
+
