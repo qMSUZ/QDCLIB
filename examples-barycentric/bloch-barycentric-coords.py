b56cdeee3a657ca43a92b39c43e81c1d7de898bc
qMSUZ <m.sawerwain@issi.uz.zgora.pl>
2024-03-26 00:38:43 +0100

    Update bloch-barycentric-coords.py
    first part of clean-ups

--- examples-barycentric/bloch-barycentric-coords.py
+++ examples-barycentric/bloch-barycentric-coords.py
@@ -36,7 +36,7 @@ import qdclib as qdcl
 import numpy as np
 
 #from sklearn import decomposition
-from sklearn import datasets
+#from sklearn import datasets
 
 
 
@@ -44,162 +44,142 @@ from sklearn import datasets
 # cleanup in progress ...
 #
 
-b = qdcl.BlochVisualization()
-#b.set_view(-20, 15)
-b.set_title("Bloch Vector Points")
-
-#b.set_pure_states( ps_d, "green") 
-#b.set_pure_states_as_vectors( centers, "red" )
-
-ptns = np.empty((0,3))
-
-ptns = np.append( ptns, 
-    [ qdcl.convert_spherical_coordinates_to_bloch_vector(
-       1.0, 
-       np.radians(45),          # (0 <= _theta <= np.pi
-       np.radians(90 + 55))  # (0 <= _phi <= 2.0 * np.pi)
-    ], axis=0 )
-
-ptns = np.append( ptns, 
-    [ qdcl.convert_spherical_coordinates_to_bloch_vector(
-       1.0, 
-       np.radians(50),          # (0 <= _theta <= np.pi
-       np.radians(90 + 17))  # (0 <= _phi <= 2.0 * np.pi)
-    ], axis=0 )
-
-ptns = np.append( ptns, 
-    [ qdcl.convert_spherical_coordinates_to_bloch_vector(
-       1.0, 
-       np.radians(60),          # (0 <= _theta <= np.pi
-       np.radians(90 + 40))  # (0 <= _phi <= 2.0 * np.pi)
-    ], axis=0 )
-
-ptns = np.append( ptns, 
-    [ qdcl.convert_spherical_coordinates_to_bloch_vector(
-       1.0, 
-       np.radians(30),          # (0 <= _theta <= np.pi
-       np.radians(90 + 30))  # (0 <= _phi <= 2.0 * np.pi)
-    ], axis=0 )
-
-# state inside polygon
-ptns = np.append( ptns, 
-    [ qdcl.convert_spherical_coordinates_to_bloch_vector(
-       1.0, 
-       np.radians(47),          # (0 <= _theta <= np.pi
-       np.radians(90 + 35))  # (0 <= _phi <= 2.0 * np.pi)
-    ], axis=0 )
-
-labels=["q0", "q1", "q2", "q3"]
-b.set_points( ptns )
-
-f=b.make_figure()
-f.show()
-
-
-b = qdcl.BlochVisualization()
-#b.set_view(-20, 15)
-
-b.set_title("")
-
+def basic_example():
+    b = qdcl.BlochVisualization()
     
-b.clear_points()
-b.clear_vectors()
-#b.enable_single_batch_draw()
-b.enable_single_batch_draw()
+    b.set_title("Bloch Vector Points")
+        
+    ptns = np.empty((0,3))
+    
+    ptns = np.append( ptns, 
+        [ qdcl.convert_spherical_coordinates_to_bloch_vector(
+           1.0, 
+           np.radians(45),          # (0 <= _theta <= np.pi
+           np.radians(90 + 55))  # (0 <= _phi <= 2.0 * np.pi)
+        ], axis=0 )
+    
+    ptns = np.append( ptns, 
+        [ qdcl.convert_spherical_coordinates_to_bloch_vector(
+           1.0, 
+           np.radians(50),          # (0 <= _theta <= np.pi
+           np.radians(90 + 17))  # (0 <= _phi <= 2.0 * np.pi)
+        ], axis=0 )
+    
+    ptns = np.append( ptns, 
+        [ qdcl.convert_spherical_coordinates_to_bloch_vector(
+           1.0, 
+           np.radians(60),          # (0 <= _theta <= np.pi
+           np.radians(90 + 40))  # (0 <= _phi <= 2.0 * np.pi)
+        ], axis=0 )
+    
+    ptns = np.append( ptns, 
+        [ qdcl.convert_spherical_coordinates_to_bloch_vector(
+           1.0, 
+           np.radians(30),          # (0 <= _theta <= np.pi
+           np.radians(90 + 30))  # (0 <= _phi <= 2.0 * np.pi)
+        ], axis=0 )
+    
+    b.set_points( ptns )
+    
+          
+    b.clear_points()
+    b.clear_vectors()
+    
+    b.enable_single_batch_draw()
+    
+    q0=qdcl.convert_bloch_vector_to_pure_state( ptns[0][0], ptns[0][1], ptns[0][2] )
+    q1=qdcl.convert_bloch_vector_to_pure_state( ptns[1][0], ptns[1][1], ptns[1][2] )
+    q2=qdcl.convert_bloch_vector_to_pure_state( ptns[2][0], ptns[2][1], ptns[2][2] )
+    q3=qdcl.convert_bloch_vector_to_pure_state( ptns[3][0], ptns[3][1], ptns[3][2] )
+     
+    qvprime1 = (q0+q1+q2+q3)
+    qvprime1 = qvprime1 / np.linalg.norm(qvprime1)
+             
+    
+    ptns1 = np.empty((0,2))
+    ptns1 = np.append(ptns1, [ q0 ], axis=0)
+    ptns1 = np.append(ptns1, [ q1 ], axis=0)
+    ptns1 = np.append(ptns1, [ q2 ], axis=0)
+    ptns1 = np.append(ptns1, [ q3 ], axis=0)
+    
+    ptns2 = np.empty((0,2))
+    ptns2 = np.append(ptns2, [ qvprime1 ], axis=0)   
+    
+    b.add_pure_states( ptns1, "red", "*" )
+    b.add_pure_states( ptns2, "green", "^" )
+    
+    f=b.make_figure()
+    f.show()
+    f.savefig("simple-example.png")
+    
 
-q0=qdcl.convert_bloch_vector_to_pure_state( ptns[0][0], ptns[0][1], ptns[0][2] )
-q1=qdcl.convert_bloch_vector_to_pure_state( ptns[1][0], ptns[1][1], ptns[1][2] )
-q2=qdcl.convert_bloch_vector_to_pure_state( ptns[2][0], ptns[2][1], ptns[2][2] )
-q3=qdcl.convert_bloch_vector_to_pure_state( ptns[3][0], ptns[3][1], ptns[3][2] )
 
-qv=qdcl.convert_bloch_vector_to_pure_state( ptns[4][0], ptns[4][1], ptns[4][2] )
- 
-qvprime1 = (q0+q1+q2+q3)
-qvprime1 = qvprime1 / np.linalg.norm(qvprime1)
+def simple_blobs_example():
+    pass
 
-qvprime2 = (q0+q3)
-qvprime2 = qvprime2 / np.linalg.norm(qvprime2)
-
-
-print(np.vdot(qv, q1))
-print(np.vdot(qv, qvprime1))
+def iris_set_example():
+    pass
 
 
 
-ptns1 = np.empty((0,2))
-ptns1 = np.append(ptns1, [ q0 ], axis=0)
-ptns1 = np.append(ptns1, [ q1 ], axis=0)
-ptns1 = np.append(ptns1, [ q2 ], axis=0)
-ptns1 = np.append(ptns1, [ q3 ], axis=0)
-
-ptns2 = np.empty((0,2))
-ptns2 = np.append(ptns2, [ qvprime1 ], axis=0)
-#ptns2 = np.append(ptns2, [ qvprime2 ], axis=0)
+basic_example()
 
 
-b.add_pure_states( ptns1, "red", "*" )
-b.add_pure_states( ptns2, "green", "^" )
+# #
+# # simple blobs
+# #
 
-f=b.make_figure()
-f.show()
-f.savefig("simple-example.png")
-
-
-#
-# simple blobs
-#
-
-n_samples = 20
-n_clusters = 2
+# n_samples = 20
+# n_clusters = 2
    
-centers = [ (1.0,  0.0), (0.0, 1.0) ]
+# centers = [ (1.0,  0.0), (0.0, 1.0) ]
 
-#d = qdcl.create_blob_2d( n_samples, centers )
-# d, org_labels = datasets.make_blobs( n_samples=n_samples, 
-#                                       centers=centers, 
-#                                       cluster_std=0.10, 
-#                                       shuffle=False, 
-#                                       random_state=1234 )
+# #d = qdcl.create_blob_2d( n_samples, centers )
+# # d, org_labels = datasets.make_blobs( n_samples=n_samples, 
+# #                                       centers=centers, 
+# #                                       cluster_std=0.10, 
+# #                                       shuffle=False, 
+# #                                       random_state=1234 )
 
-d = qdcl.create_focused_qubits_probes_with_uniform_placed_centers(n_samples, 2, 2, 0.15)
+# d = qdcl.create_focused_qubits_probes_with_uniform_placed_centers(n_samples, 2, 2, 0.15)
 
-#dnrm = qdcl.encode_probes_by_normalization(d)
-dnrm=d
+# #dnrm = qdcl.encode_probes_by_normalization(d)
+# dnrm=d
 
-labels, centroids = qdcl.quantum_kmeans( dnrm, n_samples, n_clusters, 128, qdcl.cosine_distance )
+# labels, centroids = qdcl.quantum_kmeans( dnrm, n_samples, n_clusters, 128, qdcl.cosine_distance )
 
-dnrm_set1 = dnrm[0:10]
-dnrm_set2 = dnrm[10:20]
+# dnrm_set1 = dnrm[0:10]
+# dnrm_set2 = dnrm[10:20]
 
-#f = qdcl.create_circle_plot_for_2d_data( dnrm )
-#f = qdcl.create_circle_plot_for_2d_data( dnrm_set1 )
-#f = qdcl.create_circle_plot_for_2d_data( dnrm_set2 )
+# #f = qdcl.create_circle_plot_for_2d_data( dnrm )
+# #f = qdcl.create_circle_plot_for_2d_data( dnrm_set1 )
+# #f = qdcl.create_circle_plot_for_2d_data( dnrm_set2 )
 
-b = qdcl.BlochVisualization()
-#b.set_view(-20, 15)
+# b = qdcl.BlochVisualization()
+# #b.set_view(-20, 15)
 
-b.set_title("Quantum States for two blobs")
+# b.set_title("Quantum States for two blobs")
 
-b.clear_points()
-b.clear_vectors()
-b.enable_single_batch_draw()
-#b.enable_single_batch_draw()
+# b.clear_points()
+# b.clear_vectors()
+# b.enable_single_batch_draw()
+# #b.enable_single_batch_draw()
 
-b.set_points( d )
-#b.set_points( centroids )
-b.enable_single_batch_draw()
+# b.set_points( d )
+# #b.set_points( centroids )
+# b.enable_single_batch_draw()
     
-#b.add_pure_states( dnrm_set1, "red" )
-#b.add_pure_states( centroids, "green" )
+# #b.add_pure_states( dnrm_set1, "red" )
+# #b.add_pure_states( centroids, "green" )
 
-f=b.make_figure()
-f.show()
-f.savefig("simple-blobs.png")
+# f=b.make_figure()
+# f.show()
+# f.savefig("simple-blobs.png")
 
 
-####
-# f = plt.figure()
-# f.set_rasterized(True)
-# ax = f.add_subplot(111)
-# ax.set_rasterized(True)
-# f.savefig('figure_name.eps',rasterized=True,dpi=300)
+# ####
+# # f = plt.figure()
+# # f.set_rasterized(True)
+# # ax = f.add_subplot(111)
+# # ax.set_rasterized(True)
+# # f.savefig('figure_name.eps',rasterized=True,dpi=300)
